f8cbf89e4cb92d3a6cf305131bcd34b658c7cbb1
Trigdev <ahorisaac.ai@gmail.com>
2022-10-30 11:25:14 +0100

    Project Development Completed v1.0.0

---
+++ src/context.tsx
@@ -0,0 +1,79 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useContext, useEffect, useCallback } from "react";
+
+const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+
+type AppProviderProps = {
+  children: React.ReactNode;
+};
+
+type AppContextProps = {
+  loading: boolean;
+  searchTerm: string;
+  cocktails: [];
+  setSearchTerm: React.Dispatch<React.SetStateAction<string>>;
+};
+
+export type CocktailProp = {
+  image: string;
+  name: string;
+  info: string;
+  id: string | number;
+  glass: string;
+};
+
+const AppContext = React.createContext({} as AppContextProps);
+
+const AppProvider = ({ children }: AppProviderProps) => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [searchTerm, setSearchTerm] = useState<string>("a");
+  const [cocktails, setCocktails] = useState<[]>([]);
+
+  const fetchDrinks = useCallback(async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`${url}${searchTerm}`);
+      const data = await response.json();
+      const { drinks } = data;
+      if (drinks) {
+        const newCocktails = drinks.map((item: any) => {
+          const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } =
+            item;
+          return {
+            id: idDrink,
+            name: strDrink,
+            image: strDrinkThumb,
+            info: strAlcoholic,
+            glass: strGlass,
+          };
+        });
+        setCocktails(newCocktails);
+      } else {
+        setCocktails([]);
+      }
+    } catch (error) {}
+  }, [searchTerm]);
+
+  useEffect(() => {
+    fetchDrinks();
+  }, [searchTerm, fetchDrinks]);
+
+  return (
+    <AppContext.Provider
+      value={{
+        loading,
+        searchTerm,
+        cocktails,
+        setSearchTerm,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
---
+++ src/pages/About.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ipsum
+        quam voluptatem, repellat nesciunt voluptatum dicta, illum blanditiis
+        quasi harum illo aliquid aperiam delectus veritatis sit eveniet
+        similique quod iusto nulla laborum? Laborum reiciendis in voluptatibus
+        totam! Iusto, totam animi.
+      </p>
+    </section>
+  );
+};
+
+export default About;
